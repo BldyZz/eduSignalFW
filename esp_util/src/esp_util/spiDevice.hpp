c8a53d27e419a901dd538f02381472da728bdcee
Patrick Hanner <hannerpatrick@gmail.com>
2022-05-25 23:59:14 +0200

    added SPI classes

---
+++ esp_util/src/esp_util/spiDevice.hpp
@@ -0,0 +1,53 @@
+//
+// Created by patrick on 5/25/22.
+//
+
+#pragma once
+
+#include "driver/gpio.h"
+#include "driver/spi_master.h"
+#include "spiHost.hpp"
+
+#include <cstdint>
+#include <cstring>
+#include <vector>
+
+namespace esp {
+struct spiDevice {
+private:
+    spi_device_interface_config_t interfaceConfig;
+    spi_device_handle_t           spiDeviceHandle;
+
+public:
+    explicit spiDevice(
+      spiHost const& host,
+      std::size_t const   clockSpeed,
+      gpio_num_t  const  chipSelect,
+      std::size_t const  queueSize,
+      std::uint8_t const  spiMode) {
+        std::memset(&interfaceConfig, 0, sizeof(interfaceConfig));
+        std::memset(&spiDeviceHandle, 0, sizeof(spiDeviceHandle));
+
+        interfaceConfig.clock_speed_hz  = clockSpeed;
+        interfaceConfig.mode            = spiMode;
+        interfaceConfig.spics_io_num    = chipSelect;
+        interfaceConfig.queue_size      = queueSize;
+        interfaceConfig.cs_ena_pretrans = 0;
+        esp_err_t ret             = spi_bus_add_device(host.getHostDevice(), &interfaceConfig, &spiDeviceHandle);
+        ESP_ERROR_CHECK(ret);
+    }
+
+    void sendBlocking(std::vector<std::byte> const& data){
+        spi_transaction_t t;
+        std::memset(&t, 0, sizeof(t));
+        t.length = data.size()*sizeof(std::byte);
+        t.tx_buffer = data;
+        assert(spi_device_polling_transmit(spiDeviceHandle, &t)==ESP_OK);
+    }
+
+    void sendDMA(std::vector<std::byte> const& data){
+        //TODO
+    }
+};
+
+};   // namespace esp
--- esp_util/src/esp_util/spiHost.hpp
+++ esp_util/src/esp_util/spiHost.hpp
@@ -4,19 +4,40 @@
 
 #pragma once
 
-#include "esp_util/nvs.hpp"
+#include "driver/gpio.h"
+#include "driver/spi_master.h"
 
-#include <condition_variable>
-#include <esp_event.h>
-#include <esp_netif.h>
-#include <esp_wifi.h>
-#include <mutex>
-#include <string>
+#include <cstddef>
+#include <cstring>
 
 namespace esp {
-    struct spiHost {
-    private:
-
-    };
+struct spiHost {
+private:
+    spi_bus_config_t  busConfig;
+    spi_host_device_t hostDevice;
+public:
+    explicit spiHost(
+      spi_host_device_t const& spiHostDevice,
+      gpio_num_t const        MISO,
+      gpio_num_t const        MOSI,
+      gpio_num_t const        CLK,
+      std::size_t const       transferSize,
+      spi_common_dma_t const  dmaEnable) {
+        std::memset(&busConfig, 0, sizeof(busConfig));
+        std::memset(&hostDevice, 0, sizeof(hostDevice));
+        busConfig.miso_io_num     = MISO;
+        busConfig.mosi_io_num     = MOSI;
+        busConfig.sclk_io_num     = CLK;
+        busConfig.quadhd_io_num   = -1;
+        busConfig.quadwp_io_num   = -1;
+        busConfig.max_transfer_sz = transferSize;
+        hostDevice                 = spiHostDevice;
+        auto ret                  = spi_bus_initialize(hostDevice, &busConfig, dmaEnable);
+        ESP_ERROR_CHECK(ret);
+    }
+    spi_host_device_t getHostDevice() const{
+        return hostDevice;
+    }
+};
 
 };   // namespace esp
