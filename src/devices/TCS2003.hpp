a7e3535b78753b6dd949ab77f70eef153cf322fe
Patrick Hanner <hannerpatrick@gmail.com>
2022-08-18 21:13:15 +0200

    specified Todo

--- src/devices/TCS2003.hpp
+++ src/devices/TCS2003.hpp
@@ -8,13 +8,14 @@
 #include <cstdint>
 #include "fmt/format.h"
 
-//TODO: Not working properly...
+//TODO: Not working properly... No connection to Bus
 
 template <typename I2CConfig>
 struct TCS2003 : private esp::i2cDevice<I2CConfig, 0x48>{
 
     explicit TCS2003(){
         fmt::print("TCS2003: Initializing...\n");
+        this->write(std::array{std::byte{0x00}});
     }
 
     enum class State {
